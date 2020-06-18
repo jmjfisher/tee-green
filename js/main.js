76294a314e89bfe47f2294e02414eb56de76df46
JohnMark Fisher <fisherjohnmark@gmail.com>
2020-06-18 12:14:33 -0500

    UI and JS

--- js/main.js
+++ js/main.js
@@ -1,69 +1,67 @@
-$("#number").change(function(){
+$("#f9b").click(function(){
     
-    var front = ["1","2","3","4","5","6","7","8","9"]
-    var back = ["10","11","12","13","14","15","16","17","18"]
+    $('.hole').css('display','none');
+    $('.br-div').css('display','none');
+    $('.table').hide();
     
-    if ($('#number').val() == "f9") {
-        $('.hole').css('display','none');
-        var i;
-        for (i = 0; i < front.length; i++) {
-            $("#"+front[i]).show();
-        };
-    } else if ($('#number').val() == "b9") {
-        $('.hole').css('display','none');
-        var i;
-        for (i = 0; i < front.length; i++) {
-            $("#"+back[i]).show();
-        };
-    } else if ($('#number').val() == "a18") {
-        $('.hole').css('display','none');
-        var i;
-        for (i = 0; i < front.length; i++) {
-            $("#"+front[i]).show();
-        };
-        var i;
-        for (i = 0; i < front.length; i++) {
-            $("#"+back[i]).show();
-        };
-    } else if ($('#number').val() == "refresh") {
-        $('.hole').css('display','none');
-    };
+    $('.front-9').css('display','inline');
+    $('.br-div-f').css('display','block');
+    
 });
 
-$(".hole").click(function() {
+$("#b9b").click(function(){
+    
+    $('.hole').css('display','none');
+    $('.br-div').css('display','none');
+    $('.table').hide();
+    
+    $('.back-9').css('display','inline');
+    $('.br-div-b').css('display','block');
+    
+});
+
+$(".hole").click(function(){
     
     var hole = parseInt($(this).attr("id"),10);
     
-	if ("geolocation" in navigator) {
+    function geoEnabled(position){
         
-		navigator.geolocation.getCurrentPosition(function(position){
-            
-            var userLat = position.coords.latitude;
-            var userLon = position.coords.longitude;
-            var course = $('#course').val();
-            
-            var q1 = "SELECT ST_Distance((select the_geom_webmercator ";
-            var q2 = "ST_Transform('SRID=4326;POINT("+userLon+" "+userLat+")'::geometry, 3857))*cosd("+44.928378+") as distance";
-            var qt = "from fisherjohnmark.golf where course ='"+course+"' and hole="+hole+" and torg='T'),";
-            var qg = "from fisherjohnmark.golf where course ='"+course+"' and hole="+hole+" and torg='G'),";
-            
-            var teeQuery = q1+qt+q2;
-            var greenQuery = q1+qg+q2;
-            var url = "https://fisherjohnmark.carto.com/api/v2/sql?q=";
-            
-            var teeAPI = url+teeQuery;
-            var greenAPI = url+greenQuery;
-            
-            $.getJSON(teeAPI, function(data){
-                teeYards = Math.round(1.09361*data.rows[0].distance);
-                $('#tee-text').html("<b>"+teeYards+"</b>")
-            });
-            $.getJSON(greenAPI, function(data){
-                GreenYards = Math.round(1.09361*data.rows[0].distance);
-                $('#green-text').html("<b>"+GreenYards+"</b>")
-            });
-            
-            $('.table').show();
+        var userLat = position.coords.latitude;
+        var userLon = position.coords.longitude;
+        var course = $('#course').val();
+
+        var q1 = "SELECT ST_Distance((select the_geom_webmercator ";
+        var q2 = "ST_Transform('SRID=4326;POINT("+userLon+" "+userLat+")'::geometry, 3857))*cosd("+44.928378+") as distance";
+        var qt = "from fisherjohnmark.golf where course ='"+course+"' and hole="+hole+" and torg='T'),";
+        var qg = "from fisherjohnmark.golf where course ='"+course+"' and hole="+hole+" and torg='G'),";
+
+        var teeQuery = q1+qt+q2;
+        var greenQuery = q1+qg+q2;
+        var url = "https://fisherjohnmark.carto.com/api/v2/sql?q=";
+
+        var teeAPI = url+teeQuery;
+        var greenAPI = url+greenQuery;
+
+        $.getJSON(teeAPI, function(data){
+            teeYards = Math.round(1.09361*data.rows[0].distance);
+            $('#tee-text').html("<b>"+teeYards+"</b>")
         });
-	};
+        $.getJSON(greenAPI, function(data){
+            GreenYards = Math.round(1.09361*data.rows[0].distance);
+            $('#green-text').html("<b>"+GreenYards+"</b>")
+        });
+
+        $('.table').show();
+    };
+    
+    function geoDisabled(){
+        
+        alert("Please enable your Geolocation!");
+    };
+    
+	if (navigator.geolocation){
+        
+		navigator.geolocation.getCurrentPosition(geoEnabled,geoDisabled,{enableHighAccuracy:true})
+    };
+    
 });
